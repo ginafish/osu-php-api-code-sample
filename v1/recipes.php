b25e7941b876f255b0e9d4fa9a96e2c31ff97ca3
ginafish <phipps.gina@gmail.com>
2018-02-07 09:04:24 -0800

    Testing getAll endpoint

--- v1/recipes.php
+++ v1/recipes.php
@@ -9,13 +9,24 @@ if(!empty($GET["id"])) {
     getAllRecipes();
 }
 
+
+
 function getAllRecipes() {
     global $connection;
     $query = "SELECT * FROM Calls_For";
-
+    $response = array();
+    $result = mysqli_query($connection, $query);
+    while($row = mysqli_fetch_array($result)) {
+        $response[] = $row;
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
 }
 
-function getRecipeByName($recipeName) {
+function getRecipeByName($recipeName="invalid") {
     global $connection;
+    if($recipeName == "invalid") {
+        return http_response_code(400);
+    }
     $query = "SELECT * FROM Calls_For WHERE Recipe_Name=" .$recipeName.;
 }
