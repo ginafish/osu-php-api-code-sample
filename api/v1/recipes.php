2fcc52c403cffc4ea99f9af5d73093eb8efcc3ff
ginafish <phippsg@oregonstate.edu>
2018-02-07 11:11:46 -0800

    Syntax fixes, still working on the 404

--- api/v1/recipes.php
+++ api/v1/recipes.php
@@ -18,21 +18,24 @@ function getAllRecipes() {
     $query = "SELECT * FROM Calls_For";
     $response = array();
     $result = mysqli_query($connection, $query);
-    <!-- while($row = mysqli_fetch_array($result)) {
-        $response[] = $row;
-    } -->
-    $response = 
-    mysqli_free_result($result);
+    $response = $response = mysqli_fetch_array($result);
     header('Content-Type: application/json');
     echo json_encode($response);
+    mysqli_free_result($result);
 }
 
-<!-- function getRecipeByName($recipeName="invalid") {
+function getRecipeByName($recipeName="invalid") {
     global $connection;
     if($recipeName == "invalid") {
         return http_response_code(400);
     }
-    $query = "SELECT * FROM Calls_For WHERE Recipe_Name=" .$recipeName.;
-} -->
+    $query = "SELECT * FROM Calls_For WHERE Recipe_Name=".$recipeName."";
+    $response = array();
+    $result = mysqli_query($connection, $query);
+    $response = mysqli_fetch_array($result);
+    header('Content-Type: application/json');
+    echo json_encode($response);
+    mysqli_free_result($result);
+}
 
 ?>
